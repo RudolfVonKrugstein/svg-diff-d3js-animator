739a08cf31a355f6638a0d1c03598c21e5a8cbb0
Nathan Hüsken <nathan@wintercloud.de>
2022-10-21 21:46:41 +0200

    [cicd] minimize bunlde before publishing

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,8 @@
+import { uglify } from "rollup-plugin-uglify";
+
 export default {
     input: 'out-tsc/index.js',
+    plugins: [uglify()],
     output: {
         name: "svg_diff_d3js_animator",
         file: 'bundle.js',
